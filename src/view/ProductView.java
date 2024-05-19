100ae993bd808283f3bfce29f6cb6e372935e838
TheSky189 <kenkaneki94tg@gmail.com>
2024-05-19 03:19:26 +0200

    Apply command that is needed to change about option 2,3,9. Views.

--- src/view/ProductView.java
+++ src/view/ProductView.java
@@ -3,12 +3,15 @@ package view;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
+//import javax.swing.JComponent;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import main.Shop;
 import model.Product;
+import utils.Constants;
+
 import java.awt.Font;
 
 public class ProductView extends JDialog implements ActionListener {
@@ -19,6 +22,10 @@ public class ProductView extends JDialog implements ActionListener {
     private JTextField priceField;
     private Shop shop;
     private int option;
+	private JButton okButton;
+	private JLabel stockLabel;
+	private JLabel priceLabel;
+
 
     public ProductView(Shop shop, int option) {
         this.shop = shop;
@@ -37,7 +44,7 @@ public class ProductView extends JDialog implements ActionListener {
         productNameField.setBounds(153, 121, 210, 20);
         getContentPane().add(productNameField);
 
-        JLabel stockLabel = new JLabel("Stock:");
+        stockLabel = new JLabel("Stock:");
         stockLabel.setFont(new Font("Tahoma", Font.BOLD, 13));
         stockLabel.setBounds(153, 151, 80, 20);
         getContentPane().add(stockLabel);
@@ -46,7 +53,7 @@ public class ProductView extends JDialog implements ActionListener {
         stockField.setBounds(153, 181, 210, 20);
         getContentPane().add(stockField);
 
-        JLabel priceLabel = new JLabel("Precio público:");
+        priceLabel = new JLabel("Precio público:");
         priceLabel.setFont(new Font("Tahoma", Font.BOLD, 13));
         priceLabel.setBounds(153, 211, 100, 20);
         getContentPane().add(priceLabel);
@@ -54,9 +61,10 @@ public class ProductView extends JDialog implements ActionListener {
         priceField = new JTextField();
         priceField.setBounds(153, 241, 210, 20);
         getContentPane().add(priceField);
+        
 
         // Crear boton "OK" para confirmar la operacion
-        JButton okButton = new JButton("OK");
+        okButton = new JButton("OK");
         okButton.setBounds(153, 289, 100, 30);
         getContentPane().add(okButton);
         okButton.addActionListener(this); 
@@ -71,32 +79,100 @@ public class ProductView extends JDialog implements ActionListener {
             }
         });
         
+        configureView(option);
+        
+    }
+    
+    private void configureView(int option) {
+        switch (option) {
+            case Constants.OPTION_ADD_PRODUCT:
+                priceField.setVisible(true);
+                break;
+            case Constants.OPTION_ADD_STOCK:
+                priceField.setVisible(false);
+                priceLabel.setVisible(false);
+                break;
+            case Constants.OPTION_REMOVE_PRODUCT:
+                stockField.setVisible(false);
+                stockLabel.setVisible(false);
+                priceField.setVisible(false);
+                priceLabel.setVisible(false);
+                break;
+            default:
+                break;
+        }
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        // Obtener los datos ingresados por el usuario
-        String productName = productNameField.getText();
-        int stock = Integer.parseInt(stockField.getText());
-        double price = Double.parseDouble(priceField.getText());
+        // en caso de hacer clic en el botón OK
+        if (e.getSource() == okButton) {
+            Product product;
+            switch (this.option) {
+                case Constants.OPTION_ADD_PRODUCT:
+                    // verificar que el producto no existe
+                    product = shop.findProduct(productNameField.getText());
+                    if (product != null) {
+                        JOptionPane.showMessageDialog(null, "Producto ya existe ", "Error", JOptionPane.ERROR_MESSAGE);
+                    } else {
+                        try {
+                            // Convertir el valor de Amount a double
+                            double wholesalerPrice = Double.parseDouble(priceField.getText());
+                            product = new Product(productNameField.getText(), wholesalerPrice, true, Integer.parseInt(stockField.getText()));
+                            shop.addProduct(product);
+                            JOptionPane.showMessageDialog(null, "Producto añadido ", "Information", JOptionPane.INFORMATION_MESSAGE);
+                            // liberar pantalla actual
+                            dispose();
+                        } catch (NumberFormatException ex) {
+                            JOptionPane.showMessageDialog(null, "Formato de número inválido", "Error", JOptionPane.ERROR_MESSAGE);
+                        }
+                    }
+                    break;
 
-        // Realizar la operacion correspondiente segun la opcion seleccionada
-        //switch (option) {
-         //   case 2:
-          //      addProduct(productName, stock, price);
-          //      break;
-           // case 3:
-          //      addStock(productName, stock);
-          //      break;
-          //  case 9:
-          //      removeProduct(productName);
-         //       break;
-         //   default:
-          //      break;
-       // }
+                case Constants.OPTION_ADD_STOCK:
+                    // verificar que el producto existe
+                    product = shop.findProduct(productNameField.getText());
+                    if (product == null) {
+                        JOptionPane.showMessageDialog(null, "Producto no existe ", "Error", JOptionPane.ERROR_MESSAGE);
+                    } else {
+                        try {
+                            product.setStock(product.getStock() + Integer.parseInt(stockField.getText()));
+                            JOptionPane.showMessageDialog(null, "Stock actualizado ", "Information", JOptionPane.INFORMATION_MESSAGE);
+                            // liberar pantalla actual
+                            dispose();
+                        } catch (NumberFormatException ex) {
+                            JOptionPane.showMessageDialog(null, "Formato de número inválido", "Error", JOptionPane.ERROR_MESSAGE);
+                        }
+                    }
+                    break;
+
+                case Constants.OPTION_REMOVE_PRODUCT:
+                    // verificar que el producto existe
+                    product = shop.findProduct(productNameField.getText());
+                    if (product == null) {
+                        JOptionPane.showMessageDialog(null, "Producto no existe ", "Error", JOptionPane.ERROR_MESSAGE);
+                    } else {
+                        shop.getInventory().remove(product);
+                        JOptionPane.showMessageDialog(null, "Producto eliminado", "Information", JOptionPane.INFORMATION_MESSAGE);
+                        // liberar pantalla actual
+                        dispose();
+                    }
+                    break;
+                    
+                // Extra: he intentado pero no me dio tiempo, ERROR
+                case Constants.OPTION_LOAD_INVENTORY:
+                    break;
+                    
+                default:
+                    JOptionPane.showMessageDialog(this, "Opción no válida.");
+                    break;
+            }
+        } 
     }
 
-    // Método para añadir un nuevo producto
+
+
+    // Añadir un nuevo producto
     private void addProduct(String name, int stock, double price) {
         // Verificar si el producto ya existe
         if (shop.findProduct(name) != null) {
@@ -134,18 +210,24 @@ public class ProductView extends JDialog implements ActionListener {
         }
         dispose();
     }
+    
+
 
     // Obtener el titulo de la ventana segun la opcion seleccionada
     private String getDialogTitle(int option) {
         switch (option) {
-            case 2:
-                return "Añadir producto";
-            case 3:
-                return "Añadir stock";
-            case 9:
-                return "Eliminar producto";
+            case Constants.OPTION_ADD_PRODUCT:
+                return "Añadir Producto";
+            case Constants.OPTION_ADD_STOCK:
+                return "Añadir Stock";
+            case Constants.OPTION_REMOVE_PRODUCT:
+                return "Eliminar Producto";
+                
+            // Extra: LOAD_INVENTORY no funciona.    
+            case Constants.OPTION_LOAD_INVENTORY:
+                return "Cargar Inventario";
             default:
-                return "";
+                return "Producto";
         }
     }
 }
