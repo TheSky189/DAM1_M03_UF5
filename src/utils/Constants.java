67c6e1db5b1066b3d0fb5d76c9f0a3eca2680532
TheSky189 <kenkaneki94tg@gmail.com>
2024-05-19 03:18:20 +0200

    Add new option Load Inventory.

--- src/utils/Constants.java
+++ src/utils/Constants.java
@@ -6,5 +6,7 @@ public class Constants {
 	public static final int OPTION_ADD_PRODUCT = 2;
 	public static final int OPTION_ADD_STOCK = 3;
 	public static final int OPTION_REMOVE_PRODUCT = 9;
+	
+    public static final int OPTION_LOAD_INVENTORY = 5; // Opcion extra no acabado.
 
 }
